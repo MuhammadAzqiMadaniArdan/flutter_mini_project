91483cf6f3f446999d15cc36e965e15ce0878f5b
MuhammadAzqiMadaniArdan <muhammadazqimadaniardan@smkwikrama.sch.id>
2024-12-12 07:49:45 +0700

    update data fix

--- lib/module/picture/widget/list_content_widget.dart
+++ lib/module/picture/widget/list_content_widget.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:mini_project/core.dart';
 
 class ListContentWidget extends StatelessWidget {
-  final dynamic picture; // Data gambar (sebuah map)
-  final VoidCallback onTap; // Callback saat item ditekan
+  final dynamic picture;
+  final VoidCallback onTap;
 
   const ListContentWidget({
     Key? key,
@@ -16,29 +16,43 @@ class ListContentWidget extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        elevation: 8,
         margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-        child: Column(
-          children: [
-            // Ganti Image.network dengan LoadingWidget
-            LoadingWidget(
-              imageUrl: picture['download_url'],
-              height: 200,
-              width: double.infinity,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.blue.shade500,
+                Colors.white,
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerRight, // Posisi teks di kanan
-                child: Text(
-                  "by: ${picture['author']}",
-                  style: const TextStyle(
-                    fontSize: 14.0,
-                    fontStyle: FontStyle.italic,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            children: [
+              LoadingWidget(
+                imageUrl: picture['download_url'],
+                height: 200,
+                width: double.infinity,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "by: ${picture['author']}",
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      fontStyle: FontStyle.italic,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
